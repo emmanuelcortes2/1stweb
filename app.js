c73ac96f9d231b8875160d524c2434e7a67c6a93
emmanuelcortes2 <109642972+emmanuelcortes2@users.noreply.github.com>
2022-09-12 19:41:24 -0500

    Updated Readme.md & check linters

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
+const { toggle } = require('cli-spinners');
+
 function dropFunction() {
-    document.getElementById("drop-content").classList.toggle("show");
+  document.getElementById('drop-content').classList.toggle('show');
 }
-
-// function closeFunction() {
-//     document.getElementById("drop-content").classList.toggle("close");
-// }
-
-window.onclick = function(event) {
-    if (!event.target.matches('.drop-button')) {
-        var dropdowns = document.getElementsById("drop-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-        }
-    }
+dropFunction();
+toggle.addEventListener('click', () => {
+  toggle.classList.dropFunction();
+});
