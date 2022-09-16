27c1590c1c0e0ae5ddef434cc4c0a7ee21ac6131
Marcos Gualtero Lourenzo <marcosgualtero43253@gmail.com>
2022-09-16 14:21:49 -0300

    add validate-contact-form & proper styles

--- app.js
+++ app.js
@@ -7,3 +7,24 @@ dropFunction();
 toggle.addEventListener('click', () => {
   toggle.classList.dropFunction();
 });
+
+const contactForm = document.querySelector('.contact-me');
+const email = document.querySelector('#input-email');
+const submitForm = document.querySelector('#form-submit');
+const textArea = document.querySelector('#text-area-input');
+const fullName = document.querySelector('#full-name-input')
+const regEx = /^[a-z0-9]+@[a-z0-9-]+\.[a-z0-9-.]+$/;
+
+submitForm.addEventListener ('click', (bad) => {
+  let emailAddress = email.value;
+  const displayError = document.querySelector('.display-error');
+
+  if (!regEx.test(emailAddress)){
+
+    bad.preventDefault();
+
+    displayError.style.display = 'block';
+  } else if (regEx.test(emailAddress)){
+    displayError.style.display = 'none';
+  }
+});
